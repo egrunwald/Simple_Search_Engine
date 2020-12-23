cef43bb208a3294b436a6e9b015f9cb5eefdb4a7
egrunwald <egrunwald112@gmail.com>
2020-12-22 16:00:33 -0800

    stage 2

--- src/main.kt
+++ src/main.kt
@@ -1,16 +1,47 @@
 package search
 
 fun main() {
-    val input = readLine()!!.toLowerCase().split(" ")
-    val searchBy = readLine()!!.toLowerCase()
-    var inInput = false
-    for (i in input.indices) {
-        if (input[i] == searchBy) {
-            println(i + 1)
-            inInput = true
+    var inputNum: Int
+    var employees = emptyArray<String>()
+    do {
+        inputNum = try {
+            println("Enter number of employees to add:")
+            readLine()!!.trim().toInt()
+        } catch (e: NumberFormatException) {
+            println("Not a valid input!")
+            0
         }
+    } while (inputNum  == 0)
+    println("Enter all new employees data now:")
+    for (i in 1..inputNum) {
+        employees += readLine()!!
     }
-    if (!inInput) {
-        println("Not found")
+    println()
+    do {
+        inputNum = try {
+            println("Enter the number of search queries:")
+            readLine()!!.trim().toInt()
+        } catch (e: NumberFormatException) {
+            println("Not a valid input!")
+            0
+        }
+    } while (inputNum == 0)
+    for (i in 1..inputNum) {
+        println()
+        println("Enter data to search employees:")
+        val searchBy = readLine()!!.toLowerCase()
+        var employeesFound = emptyArray<String>()
+        for (j in employees.indices) {
+            if (employees[j].toLowerCase().indexOf(searchBy) != -1) {
+                employeesFound += employees[j]
+            }
+        }
+        if (employeesFound.isNotEmpty()) {
+            println()
+            println("Found employees:")
+            for (element in employeesFound) {
+                println(element)
+            }
+        } else println("No matching employee found.")
     }
 }
